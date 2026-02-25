917d3cdf2f0cb0660d82d22d7d2d6ce485fbf5b7
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-02-25 23:32:24 +0200

    header demo version

--- IceCream/src/components/LayOut/Header/Header.tsx
+++ IceCream/src/components/LayOut/Header/Header.tsx
@@ -1,25 +1,30 @@
 import logo from "./logo.svg";
-import Logo from "./logo.svg";
+import row from './rowNav.svg'
+import search from './search.svg'
+import basket from './Basket.svg'
+import rowContactUs from './rowContactUs.svg'
 import style from './_header.module.scss'
 const Header = () => {
   return (
     <header className={style.header}>
       <div className={style.header_Container}>
         <div className={style.header_Container_LogoContainer}>
-          <Logo/>
           <img className={style.header_Container_LogoContainer_logo} src={logo} alt="Logo" />
           <h2 className={style.header_Container_LogoContainer_Title}> <span className={style.spanHeader}>Icy</span>Tales</h2>
         </div>
         <div className={style.header_Container_rightContainer}>
           <nav className={style.header_Container_rightContainer_navigatePanel}>
             <ul className={style.header_Container_rightContainer_navigatePanel_list}>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Home</li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Home <img src={row} alt="rowNav" /></li>
               <li className={style.header_Container_rightContainer_navigatePanel_list_item}>About Us</li>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Pages</li>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Blog</li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Pages <img src={row} alt="rowNav" /></li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Blog <img src={row} alt="rowNav" /></li>
               <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Faq's</li> 
             </ul>
           </nav>
+          <img src={search} alt="" />
+          <img src={basket} alt="" />
+          <button className={style.header_Container_rightContainer_ContactUsButton}>Contact Us <img src={rowContactUs} alt="" /></button>
         </div>
       </div>
     </header>
