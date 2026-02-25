d62c8e673b1aad50ef0b133a6858adc6ae28dd6d
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-02-25 17:11:30 +0200

    start

--- IceCream/src/components/LayOut/Header/Header.tsx
+++ IceCream/src/components/LayOut/Header/Header.tsx
@@ -1,4 +1,15 @@
+// import Logo from "./logo.svg?react";
 const Header = () => {
-    return <></>
-}
+  return (
+    <></>
+    // <header className={style.header}>
+    //   <div className={style.header_Container}>
+    //     <div className={style.header_Container_LogoContainer}>
+    //         <Logo className={style.header_Container_LogoContainer_logo}/>
+    //         <h2 className={style.header_Container_LogoContainer_Title}> <span className={style.spanHeader}>Icy</span>Tales</h2>
+    //     </div>
+    //   </div>
+    // </header>
+  );
+};
 export default Header;
