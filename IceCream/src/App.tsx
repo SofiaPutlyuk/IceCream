b33857ef87bd2f025554a5d6b85aeb802bb2fb08
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-02-25 21:51:25 +0200

    Logo + nav

--- IceCream/src/App.tsx
+++ IceCream/src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css'
-// import Header from './components/LayOut/Header/Header'
+import Header from './components/LayOut/Header/Header'
 function App() {
 
   return (
     <>
-    {/* <Header/> */}
+    <Header/>
     </>
   )
 }
--- IceCream/src/components/LayOut/Header/Header.tsx
+++ IceCream/src/components/LayOut/Header/Header.tsx
@@ -1,15 +1,28 @@
-// import Logo from "./logo.svg?react";
+import logo from "./logo.svg";
+import Logo from "./logo.svg";
+import style from './_header.module.scss'
 const Header = () => {
   return (
-    <></>
-    // <header className={style.header}>
-    //   <div className={style.header_Container}>
-    //     <div className={style.header_Container_LogoContainer}>
-    //         <Logo className={style.header_Container_LogoContainer_logo}/>
-    //         <h2 className={style.header_Container_LogoContainer_Title}> <span className={style.spanHeader}>Icy</span>Tales</h2>
-    //     </div>
-    //   </div>
-    // </header>
+    <header className={style.header}>
+      <div className={style.header_Container}>
+        <div className={style.header_Container_LogoContainer}>
+          <Logo/>
+          <img className={style.header_Container_LogoContainer_logo} src={logo} alt="Logo" />
+          <h2 className={style.header_Container_LogoContainer_Title}> <span className={style.spanHeader}>Icy</span>Tales</h2>
+        </div>
+        <div className={style.header_Container_rightContainer}>
+          <nav className={style.header_Container_rightContainer_navigatePanel}>
+            <ul className={style.header_Container_rightContainer_navigatePanel_list}>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Home</li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>About Us</li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Pages</li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Blog</li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Faq's</li> 
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 };
 export default Header;
