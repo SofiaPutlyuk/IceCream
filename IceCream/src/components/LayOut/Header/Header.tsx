c96ebe86676b1e19b585873a2ccc514627e7e33f
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-02-27 00:26:20 +0200

    header

--- IceCream/src/components/LayOut/Header/Header.tsx
+++ IceCream/src/components/LayOut/Header/Header.tsx
@@ -1,10 +1,23 @@
 import logo from "../../../../public/assets/logo.svg";
-import row from './rowNav.svg'
-import search from './search.svg'
-import basket from './Basket.svg'
-import rowContactUs from './rowContactUs.svg'
+import row from '../../../../public/assets/rowNav.svg'
+import search from '../../../../public/assets/search.svg'
+import basket from '../../../../public/assets/Basket.svg'
+import rowContactUs from '../../../../public/assets/rowContactUs.svg'
 import style from './_header.module.scss'
+import { FaUser } from "react-icons/fa";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import user from "../../../../public/assets/user-circle-svgrepo-com.svg"
 const Header = () => {
+  const [homeList,setHomeList] = useState(false)
+  const [pagesList,setPagesList] = useState(false)
+  const toggleHomeList = () => {
+    setHomeList(!homeList)
+  }
+  const togglePagesList = () => {
+    setPagesList(!pagesList)
+  }
+   const redirectOnPage = useNavigate()
   return (
     <header className={style.header}>
       <div className={style.header_Container}>
@@ -15,16 +28,36 @@ const Header = () => {
         <div className={style.header_Container_rightContainer}>
           <nav className={style.header_Container_rightContainer_navigatePanel}>
             <ul className={style.header_Container_rightContainer_navigatePanel_list}>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Home <img src={row} alt="rowNav" /></li>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>About Us</li>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Pages <img src={row} alt="rowNav" /></li>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Blog <img src={row} alt="rowNav" /></li>
-              <li className={style.header_Container_rightContainer_navigatePanel_list_item}>Faq's</li> 
+              <li className={`${style.header_Container_rightContainer_navigatePanel_list_item} ${style.homeLink}`} onClick={toggleHomeList} style={{color: homeList ? '#F83D8E' : '#0F0200'}}>Home <img src={row} alt="rowNav" style={{fill: homeList ? '#F83D8E' : '#0F0200'}}/></li>
+              <ul className={style.homeList} style={{display: homeList ? "block" : "none"}}>
+                <li className={style.homeList_Item}>Favourites</li>
+                <li className={style.homeList_Item}>Categories</li>
+                <li className={style.homeList_Item}>Summer Offers</li>
+                <li className={style.homeList_Item}>Best Sellers</li>
+                <li className={style.homeList_Item}>Follow Us</li>
+              </ul> 
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item} onClick={() => redirectOnPage("/aboutUs")}>About Us</li>
+              <div className={style.header_Container_rightContainer_navigatePanel_list_pagesContainer} onClick={togglePagesList}>
+              <li className={`${style.header_Container_rightContainer_navigatePanel_list_item} ${style.pagesLink}`} style={{color: pagesList ? '#F83D8E' : '#0F0200'}} >Pages <img src={row} alt="rowNav" /></li>
+                <ul className={style.pagesList} style={{display: pagesList ? "block" : "none"}}>
+                  <li className={style.pagesList_Item}>Vegan</li>
+                  <li className={style.pagesList_Item}>Artisan</li>
+                </ul> 
+              </div>
+              <li className={`${style.header_Container_rightContainer_navigatePanel_list_item} ${style.BlogLink}`} onClick={() => redirectOnPage("/Blog")} >Blog</li>
+              <li className={style.header_Container_rightContainer_navigatePanel_list_item} onClick={() => redirectOnPage("/Faq")}>Faq's</li> 
             </ul>
           </nav>
-          <img src={search} alt="" />
-          <img src={basket} alt="" />
-          <button className={style.header_Container_rightContainer_ContactUsButton}>Contact Us <img src={rowContactUs} alt="" /></button>
+
+          <img src={search} alt="search" />
+          <button className={style.header_Container_rightContainer_userContainer}>
+            <img className={style.header_Container_rightContainer_useIcon} src={user} alt="" />  
+          </button>
+          <div className={style.header_Container_rightContainer_basketContainer}>
+            <img  className={style.header_Container_rightContainer_basketContainer_icon}src={basket} alt="basket" />
+            <div className={style.header_Container_rightContainer_basketContainer_counter}>0</div>
+          </div>
+          <button className={style.header_Container_rightContainer_ContactUsButton} onClick={() => redirectOnPage("/ContactUs")}>Contact Us <img src={rowContactUs} alt="" /></button>
         </div>
       </div>
     </header>
