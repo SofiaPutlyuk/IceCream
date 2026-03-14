d89a610377ff5dabaf77b0e8757559d9aa09c949
Serhii-Zamorylo23 <zamorylo@i.ua>
2026-03-14 22:02:13 +0200

    .

--- IceCream/src/redux/store.ts
+++ IceCream/src/redux/store.ts
@@ -4,8 +4,7 @@ import storage from "redux-persist/lib/storage";
 const rootReducer = combineReducers({
     temp: (state = {}) => state
 })
-
-const persistConfig = {
+    const persistConfig = {
     key: "root",
     storage,
     whitelist: [""]
